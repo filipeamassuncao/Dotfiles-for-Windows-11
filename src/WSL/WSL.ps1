49336d9864548e8744ffdb873a2abdd396f59769
Filipe Assunção <fassuncao@brndts.com>
2024-05-20 14:40:57 +0100

    Changed wsl distro to debian

--- src/WSL/WSL.ps1
+++ src/WSL/WSL.ps1
@@ -246,7 +246,7 @@ function Set-Zsh-As-Default-In-Ubuntu {
 }
 
 choco install -y "wsl2" --params "/Version:2 /Retry:true";
-choco install -y "wsl-ubuntu-2004" --params "/InstallRoot:true" --execution-timeout 3600;
+choco install -y "wsl-debiangnulinux" --params "/InstallRoot:true" --execution-timeout 3600;
 
 Update-Ubuntu-Packages-Repository;
 Update-Ubuntu-Packages;
